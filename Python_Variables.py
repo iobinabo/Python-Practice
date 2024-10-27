265d6530357103d943aa833abae124aacb77f1f1
Ifunanya Obinabo <iobinabo247@gmail.com>
2024-10-27 16:31:47 -0500

    test 2

--- Python_Variables.py
+++ Python_Variables.py
@@ -5,5 +5,9 @@ Created: 10/27/24
 https://www.geeksforgeeks.org/python-basics/#writing-your-first-python-program
 '''
 
+'''
+must startr with letter or _
+case sensitive
+'''
 var = "Geeks for geeks"
 print (var)
