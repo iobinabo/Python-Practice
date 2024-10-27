503172fc5feae96d5d495e25e03d467dbff6a4bc
Ifunanya Obinabo <iobinabo247@gmail.com>
2024-10-27 16:41:45 -0500

    test  3

--- Python_Variables.py
+++ Python_Variables.py
@@ -11,3 +11,18 @@ case sensitive
 '''
 var = "Geeks for geeks"
 print (var)
+
+#usable names
+woah = 100
+AYYY = 1
+geek = 5
+_this = 6
+H_I = 8
+print (woah, AYYY, geek, _this, H_I)
+
+#string & variable
+print("How many tried did Git/VS take to figure out?", woah)
+
+#multiple
+a = b = c = 10
+print (a, b, c)
